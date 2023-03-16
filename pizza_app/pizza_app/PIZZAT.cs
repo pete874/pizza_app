ea38e82cfb3a7c768c7ea0eb98cbddf45034acd3
Petri Nurmi <pete874@hotmail.com>
2023-03-16 11:04:24 +0200

    .

---
+++ pizza_app/pizza_app/PIZZAT.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace pizza_app
+{
+    class PIZZAT
+    {
+        YHDISTA yhteys = new YHDISTA();
+        public DataTable HaePizzat()
+        {
+            // tehdään uusi mysqlcommand. Sulkeiden sisään tulee itse komento ja otaYhteys funktio. MySqlCommand(String, MySqlConnection)
+            MySqlCommand haeKaikki = new MySqlCommand("SELECT pizza FROM pizzat", yhteys.otaYhteys());
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable taulu = new DataTable();
+
+            adapter.SelectCommand = haeKaikki;
+            adapter.Fill(taulu);
+
+            return taulu;
+        }
+
+        public DataTable HaeTäytteet()
+        {
+            // tehdään uusi mysqlcommand. Sulkeiden sisään tulee itse komento ja otaYhteys funktio. MySqlCommand(String, MySqlConnection)
+            MySqlCommand haeKaikki = new MySqlCommand("SELECT aines FROM ainekset", yhteys.otaYhteys());
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable taulu = new DataTable();
+
+            adapter.SelectCommand = haeKaikki;
+            adapter.Fill(taulu);
+
+            return taulu;
+        }
+    }
+}
